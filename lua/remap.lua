27e347b4c837cc7613deb87d9b48666c16fc3d79
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2025-11-23 02:59:32 +0100

    08: No more surprise macro recordings.

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,5 +1,8 @@
 vim.api.nvim_set_keymap('i', '<C-z>', '<nop>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-z>', '<nop>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Tab>', '<nop>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'qq', '<nop>', { noremap = true })
+
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>vv", vim.cmd.Ex)
@@ -16,9 +19,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("i", "<C-a>", "<Esc>A", { noremap = true })
-vim.keymap.set("i", "<C-i>", "<Esc>I", { noremap = true })
 vim.keymap.set("n", "<C-a>", "<Esc>A", { noremap = true })
-vim.keymap.set("n", "<C-i>", "<Esc>I", { noremap = true })
+vim.keymap.set("i", "<C-o>", "<Esc>o", { noremap = true })
+vim.keymap.set("i", "<C-O>", "<Esc>O", { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-H>', '<C-w>', { noremap = true })
 
 vim.keymap.set("n", "Q", "<nop>")
 
@@ -30,3 +34,5 @@ vim.keymap.set("n", "<S-C-j>", "<cmd>lprev<CR>zz")
 -- Insert empty line without entering insert mode
 vim.keymap.set('n', '<leader>o', ':<C-u>call append(line("."), repeat([""], v:count1))<CR>')
 vim.keymap.set('n', '<leader>O', ':<C-u>call append(line(".")-1, repeat([""], v:count1))<CR>')
+
+vim.api.nvim_set_keymap('n', '<C-f>', 'gg=G', { noremap = true })
